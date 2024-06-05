f0d890dca7543a972bfb6d13ccf8acf94ea13efc
Kiggundu Sulaiman <kiggundusulaiman332@gmail.com>
2024-06-05 12:13:42 +0300

    all of us

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -11,13 +11,13 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-        backgroundColor: Color.fromARGB(255, 3, 9, 17),
+        backgroundColor: Color.fromARGB(255, 6, 18, 33),
         child: Column(
           children: [
             DrawerHeader(
               child: Icon(
-                Icons.menu_open_outlined,
-                size: 50,
+                Icons.shopify_outlined,
+                size: 100,
                 color: Colors.white,
               ),
 
@@ -25,17 +25,14 @@ class FirstPage extends StatelessWidget {
 
             ListTile(
               leading: Icon(Icons.home, color: Colors.white),
-              title: Text('Home', style: TextStyle(color: Colors.white)),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FirstPage()),  // Assuming you have a HomePage defined similarly
-                );
+              title: Text('HOME', style: TextStyle(color: Colors.white)),
+              onTap: (){ Navigator.pop(context);
+              Navigator.pushNamed(context, "/index");
               },
             ),
             ListTile(
               leading: Icon(Icons.person, color: Colors.white),
-              title: Text('My Info', style: TextStyle(color: Colors.white)),
+              title: Text('MY INFO', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.push(
                   context,
@@ -46,7 +43,7 @@ class FirstPage extends StatelessWidget {
             
             ListTile(
               leading: Icon(Icons.card_giftcard, color: Colors.white),
-              title: Text('Offers', style: TextStyle(color: Colors.white)),
+              title: Text('OFFERS', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.push(
                   context,
@@ -57,7 +54,7 @@ class FirstPage extends StatelessWidget {
 
             ListTile(
               leading: Icon(Icons.settings, color: Colors.white),
-              title: Text('Settings', style: TextStyle(color: Colors.white)),
+              title: Text('SETTINGS', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.push(
                   context,
@@ -67,7 +64,7 @@ class FirstPage extends StatelessWidget {
             ),
             ListTile(
               leading: Icon(Icons.login, color: Colors.white),
-              title: Text('LogIn', style: TextStyle(color: Colors.white)),
+              title: Text('LOGIN', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.push(
                   context,
@@ -81,39 +78,29 @@ class FirstPage extends StatelessWidget {
       ),
       appBar: AppBar(
         title: Text(
-          "Shoppie",
+          "SHOPPIE",
           style: TextStyle(
             color: Color.fromARGB(255, 222, 225, 230),
             fontSize: 30,
             fontWeight: FontWeight.bold,
           ),
         ),
-      backgroundColor: Color.fromARGB(255, 1, 4, 9),
-        leading: Builder(
-          builder: (context) {
-            return GestureDetector(
-              onTap: () {
-                Scaffold.of(context).openDrawer();
-              },
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(''), // Replace with your image URL
-              ),
-            );
-          },
-        ),
+      backgroundColor: Color.fromARGB(255, 3, 16, 37),
+        leading: Icon(Icons.menu,
+        color:Colors.white ,),
         actions: [
           IconButton(
             onPressed: () {},
             icon: Icon(
-              Icons.shopping_cart,
+              Icons.notifications_active,
               color: Colors.white,
             ),
           )
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color.fromARGB(255, 1, 4, 9),
-        selectedItemColor: Color.fromARGB(255, 222, 225, 230),
+        backgroundColor: Color.fromARGB(255, 10, 25, 50),
+        selectedItemColor: Color.fromARGB(255, 35, 50, 75),
         unselectedItemColor: Colors.grey,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
